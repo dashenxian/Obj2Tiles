866861d800154799332fe506225e3681cff8d505
xiaoshenxian <125880321@qq.com>
2024-02-22 16:29:10 +0800

    增加调用入口

---
+++ GDObj2Tiles.Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using Obj2Tiles;
+
+namespace GDObj2Tiles.Test
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            args = new[] {
+                "--yuptozup", "false",
+                "--use-system-temp","false",
+                "--lat", "29.6456337183119", 
+                "--lon", "106.5632975756124", 
+                "-l", "1", 
+                "-d", "2",
+                "E:\\DeskTop\\3dloupan\\test\\ceModel\\BasicsOfRules_0.obj",
+                
+                "E:\\DeskTop\\3dloupan\\test\\output1" };
+            var entry = new Obj2TilesEntry();
+            await entry.Run(args);
+        }
+    }
+}
---
+++ Obj2Tiles/Obj2TilesEntry.cs
@@ -0,0 +1,28 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obj2Tiles
+{
+    /// <summary>
+    /// 库调用入库
+    /// </summary>
+    public class Obj2TilesEntry
+    {
+        public async Task Run(Options opts)
+        {
+            await Program.Run(opts);
+        }
+        public async Task Run(string[] args)
+        {
+            var oResult = await Parser.Default.ParseArguments<Options>(args).WithParsedAsync(Run);
+            if (oResult.Tag == ParserResultType.NotParsed)
+            {
+                throw new ArgumentException("参数输入错误，要知道详细的参数用法请调用Run(Options opts)重载");
+            }
+        }
+    }
+}
